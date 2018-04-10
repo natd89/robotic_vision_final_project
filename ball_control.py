808439565155e3cd6118cb4816a374c4e2acf6a1
natd89 <nathanday89@gmail.com>
2018-04-10 21:03:50 +0000

    stuff is kind of working. everything is communicating

--- ball_control.py
+++ ball_control.py
@@ -1,24 +1,29 @@
 import rospy
 import numpy as np
 import control
+from pdb import set_trace as pause
 from std_msgs.msg import Float64MultiArray
 
 class bc():
 
     def __init__(self):
-        self.ball_pos
-        self.xhat
-        self.xd
+        self.ball_pos = []
+        self.xhat = []
+        self.xd = [0,0]
         self.dt = 1/30.
         self.rate = rospy.Rate(30)
         self.flag = 1
         self.commands = Float64MultiArray()
+        self.g = 9.81
         
         # initialize subscriber and publisher
         # subscribe to message from camera position estimate
         rospy.Subscriber('/ball_pos', Float64MultiArray, self.callback)
         # publish servo commands based on calculated control input
         self.pub = rospy.Publisher('/servo_commands', Float64MultiArray, queue_size=1)
+
+        while len(self.ball_pos)==0:
+            a = 1
 
     def callback(self, msg):
         temp = msg.data
@@ -34,19 +39,17 @@ class bc():
         B = np.array([[0,0],
                       [0,0],
                       [5/7.*self.g,0],
-                      [0,5/7.*self.g]])
+                      [0,-5/7.*self.g]])
         
-        Q = np.array([[1.,0,0,0],
-                      [0,1.,0,0],
-                      [0,0,1.,0],
-                      [0,0,0,1.]])
+        Q = np.array([[.01,0,0,0],
+                      [0,.01,0,0],
+                      [0,0,.005,0],
+                      [0,0,0,.005]])
         
-        R = np.array([[100.,0,0,0],
-                      [0,100.,0,0],
-                      [0,0,100.,0],
-                      [0,0,0,100.]])
+        R = np.array([[100.,0],
+                      [0,100.]])
 
-        self.K = contro.lqr(A,B,Q,R)
+        self.K = control.lqr(A,B,Q,R)
 
         
     def run(self):
@@ -63,13 +66,13 @@ class bc():
             ball_vel = (self.ball_pos-pos_prev)/self.dt
             
             # build the state vector
-            xhat = np.array([[self.ball_pos[0]-self.xd[0]],
-                             [self.ball_pos[1]-self.xd[1]],
-                             [ball_vel[0]],
-                             [ball_vel[1]]])
+            xhat = np.array([[self.ball_pos[0][0]-self.xd[0]],
+                             [self.ball_pos[1][0]-self.xd[1]],
+                             [ball_vel[0][0]],
+                             [ball_vel[1][0]]])
 
             # compute the desired servo outputs
-            u = np.matmul(-self.K,xhat)                    
+            u = np.matmul(-self.K[0],xhat)                    
             
             self.commands.data = [u[0],u[1]]
             self.pub.publish(self.commands)
